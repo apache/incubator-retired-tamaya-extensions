9524ac3d7182978e425e47c060f80e79fd7c1011
Hugo Hirsch <github@hugo-hirsch.de>
2019-02-23 21:32:28 +0100

    TAMAYA-378: Fixing checkstyle issues.

--- modules/injection/injection-api/src/main/java/org/apache/tamaya/inject/spi/AbsoluteKeyResolver.java
+++ modules/injection/injection-api/src/main/java/org/apache/tamaya/inject/spi/AbsoluteKeyResolver.java
@@ -20,9 +20,7 @@ package org.apache.tamaya.inject.spi;
 
 import org.apache.tamaya.inject.api.KeyResolver;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Member;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
